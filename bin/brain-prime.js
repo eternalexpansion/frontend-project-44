da044d3fbf8f74af0026523ba1ea8e84e3eb53b6
gelegenheit <magnetism897@gmil.com]>
2024-02-26 12:23:33 +0300

    task 9 completed

---
+++ bin/brain-prime.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import gameStruct from './gamestructure.js';
+
+const desc = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+function task() {
+    const num = Math.floor(Math.random() * 10);
+    let a = 'no';
+    for(let i = 2, sq = Math.sqrt(num); i <= sq; i++) {
+        if(num % i === 0) {
+            a = 'yes';
+            break;
+        }
+    }
+
+    return [num, a];
+};
+
+gameStruct(desc, task);
