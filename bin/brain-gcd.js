34fc7e4f3be48adcb5aa0e735ab0410d67371fcf
gelegenheit <magnetism897@gmil.com]>
2024-02-26 05:32:15 +0300

    tas7 completed

---
+++ bin/brain-gcd.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import gameStruct from './gamestructure.js';
+
+const desc = 'Find the greatest common divisor of given numbers.';
+
+function obamaSus() {
+  const num1 = Math.floor(Math.random() * 10);
+  const num2 = Math.floor(Math.random() * 10);
+  let a = num1;
+  let b = num2;
+  while (a !== 0 && b !== 0) {
+    if (a >= b) {
+      a -= b;
+    } else {
+      b -= a;
+    }
+  }
+  const answer = `${a + b}`;
+
+  return [`${num1} ${num2}`, answer];
+}
+
+gameStruct(desc, obamaSus);
